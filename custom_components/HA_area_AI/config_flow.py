40cbf3e681990c8c40a8a9b21190128cf1aab19d
Dan Hajduk <Dan.hajduk@gmail.com>
2021-06-09 19:16:04 +0300

    remove unique ID

--- custom_components/HA_area_AI/config_flow.py
+++ custom_components/HA_area_AI/config_flow.py
@@ -4,6 +4,4 @@ from .const import DOMAIN
 
 class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
-    await self.async_set_unique_id(device_unique_id)
-    self._abort_if_unique_id_configured()
 
