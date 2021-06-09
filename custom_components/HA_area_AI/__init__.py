a8d63ffb3bb567e52663aa655277ba76ddb882dd
Dan Hajduk <Dan.hajduk@gmail.com>
2021-06-09 17:35:25 +0300

    Update __init__.py

--- custom_components/HA_area_AI/__init__.py
+++ custom_components/HA_area_AI/__init__.py
@@ -2,7 +2,7 @@ DOMAIN = "ha_area_ai"
 
 
 async def async_setup(hass, config):
-    hass.states.async_set("ha_area_ai.world", "Dan hajduk")
+    hass.states.async_set("ha_area_ai.world", "Paulus")
 
     # Return boolean to indicate that initialization was successful.
     return True
