2b74f8b7b650eae85276780fcea250bde180b737
Dan Hajduk <Dan.hajduk@gmail.com>
2021-06-09 18:49:08 +0300

    Uniqe ID test

--- custom_components/HA_area_AI/config_flow.py
+++ custom_components/HA_area_AI/config_flow.py
@@ -4,3 +4,6 @@ from .const import DOMAIN
 
 class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
+    await self.async_set_unique_id(device_unique_id)
+    self._abort_if_unique_id_configured()
+
